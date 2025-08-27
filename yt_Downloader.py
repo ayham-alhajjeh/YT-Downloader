bed88f26253d95dfb0495dd37c1c7417efdf1e0a
ayhamalhajjeh <168569583+ayhamalhajjeh@users.noreply.github.com>
2025-08-27 15:33:26 -0500

    Show title/views output and switch from pytube to pytubefix

--- yt_Downloader.py
+++ yt_Downloader.py
@@ -5,6 +5,16 @@ Created: 2025-08-27
 Description: Simple Python script to download YouTube videos using pytube.
 """
 
+from pytubefix import YouTube
+from sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+
+print("Title: ", yt.title)
+print("Views: ", yt.views)
+
+
 
 
 
