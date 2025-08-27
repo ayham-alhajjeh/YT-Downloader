8383aacbc048d04d124bb34257ad8c7c0ea17e50
ayhamalhajjeh <168569583+ayhamalhajjeh@users.noreply.github.com>
2025-08-27 15:52:25 -0500

    Added a progress bar to video download

--- yt_Downloader.py
+++ yt_Downloader.py
@@ -7,9 +7,23 @@ Description: Simple Python script to download YouTube videos using pytube.
 
 from pytubefix import YouTube
 from sys import argv
+import sys
+
+def progress_func(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percent = int(bytes_downloaded / total_size * 100)
+    bar = "=" * (percent // 2) + "-" * (50 - percent // 2)
+    sys.stdout.write(f"\r[{bar}] {percent}%")
+    sys.stdout.flush()
+
+def complete_func(stream, file_path):
+    print(f"\nDownload Complete: {file_path}")
+
+
 
 link = argv[1]
-yt = YouTube(link)
+yt = YouTube(link, on_progress_callback=progress_func, on_complete_callback=complete_func)
 
 print("Title: ", yt.title)
 print("Views: ", yt.views)
